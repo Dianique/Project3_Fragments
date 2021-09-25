e6f39c4672208dfba8d8dd4267c209632657d8df
Dianique <ashleydianique@yahoo.com>
2021-09-25 19:46:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/project3redo/FragmentTwo.kt
@@ -0,0 +1,84 @@
+package com.example.project3redo
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import org.w3c.dom.Text
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentTwo.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentTwo : Fragment() {
+    val args: FragmentTwoArgs by navArgs()
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("app", "get data back")
+        val getData: EditText = view.findViewById(R.id.edit_task)
+        val task = args.editData
+        getData.setText(task)
+
+        view.findViewById<Button>(R.id.save_task).setOnClickListener{
+        val updateData = view.findViewById<EditText>(R.id.edit_task).text.toString()
+        val theReceiver = FragmentTwoDirections.actionFragmentTwoToFragmentOne(updateData)
+        findNavController().navigate(theReceiver)
+
+      //  navController.setGraph(R.navigation.nav_graph, args)
+
+          }
+
+    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentTwo.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FragmentTwo().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
